fceb7385bba3b1f1a0c2d4faa775bb5745a773b4
YasmineRico <ricolyasmine@gmail.com>
2022-11-07 18:33:42 -0600

    change color scheme

--- dragon.js
+++ dragon.js
@@ -148,7 +148,7 @@ function sellWeapon() {
 	if (inventory.length > 1) {
 		gold += 15;
 		goldText.innerText = gold;
-        let currentWeapon = inventory.shift(); //takes out the weapon chose from array
+        let currentWeapon = inventory.shift();
         text.innerText = "You sold a " + currentWeapon + ".";
         text.innerText += " In your inventory you have: " + inventory;
 	} else {
